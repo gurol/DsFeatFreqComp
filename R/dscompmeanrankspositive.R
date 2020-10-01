5f349d3c365bd55b523124aed2e02423addfceac
Gürol Ca <gurol44@gmail.com>
2020-10-01 15:20:58 +0300

    Update dscompmeanrankspositive.R

--- R/dscompmeanrankspositive.R
+++ R/dscompmeanrankspositive.R
@@ -1,6 +1,6 @@
 #' Pairwise Dataset Comparison of Mean Ranks (Malware)
 #'
-#' Data compiled from academic sources for "Gaining New Insight in Datasets via Multiple Binary-Feature Frequency Ranks with A Benign/Malign Mobile Apps Example" academic study by Gurol Canbek
+#' Data compiled from academic sources for "Gaining New Insight in Machine-Learning Datasets via Multiple Binary-Feature Frequency Ranks with a Mobile Benign/Malware Apps Example" academic study by Gurol Canbek
 #'
 #' @docType data
 #'
@@ -8,10 +8,10 @@
 #'
 #' @keywords datasets, Android, malware, mobile malware
 #'
-#' @references G. Canbek et al. (2018) Gaining New Insight in Datasets via Multiple Binary-Feature Frequency Ranks with A Benign/Malign Mobile Apps Example, Journal of Machine Learning Research (JMLR) (Submitted)
+#' @references G. Canbek, (2020) Gaining Insights in Datasets in the Shade of “Garbage In, Garbage Out” Rationale: Feature-Space Distribution Fitting
 #' (\href{http://gurol.canbek.com/Publications/}{Publications by Gurol Canbek})
 #'
-#' @source \href{http://canbek.com/datasets}{Datasets by Gurol Canbek}
+#' @source \href{http://dx.doi.org/10.17632/ptd9fnsrtr.1}{Mendeley Data by Gurol Canbek}
 #'
 #' @examples
 #' data(dscompmeanrankspositive)
